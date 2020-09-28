581e82ba9d7912a4bb32f5930055bf6f028d3ac7
Dipto <diptobiswas1998@gmail.com>
2020-09-28 20:20:49 +0530

    [Dipto] refactor: Checking Attendance

--- EmployeeWageProblemFinal/EmployeeWageProblemFinal/Program.cs
+++ EmployeeWageProblemFinal/EmployeeWageProblemFinal/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace EmployeeWageProblemFinal
+namespace Welcome
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Welcome msg
             Console.WriteLine("Welcome to the Employee Wage Problem!");
+            //Check
+            int IsFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
         }
     }
 }
